dbb588df3d7360a337d87c881ce911699a26e4f8
Prasad Vallabhaneni <vallabhanenivenkataprasad@gmail.com>
2020-08-16 15:54:54 +0530

    first voice search bbc news

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from './components/newscards/NewsCards'; 
 
 const alankey =
   "e88057dd40e516e02661bff88b26121c2e956eca572e1d8b807a3e2338fdd0dc/stage";
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: alankey,
       onCommand: ({ command, articles }) => {
         if (command === "newHeadlines") {
-         console.log(articles)
+          setNewsArticles(articles);
         }
       },
     });
   }, []);
-  return <div>Voice Controll New App</div>;
+  return (
+    <div>
+      <h1>Voice Controll News App</h1>
+      <NewsCards articles={newsArticles}></NewsCards>
+    </div>
+  );
 };
 
 export default App;
