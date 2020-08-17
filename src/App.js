1296d2e7dbe9abb2497913101b4fb9cd549597b4
Prasad Vallabhaneni <vallabhanenivenkataprasad@gmail.com>
2020-08-17 22:21:17 +0530

    complete app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from './components/newscards/NewsCards'; 
 // import classes from "*.module.css";
 import useStyles from './styles';
+import wordsToNumbers from 'words-to-numbers';
 
 const alankey =
   "e88057dd40e516e02661bff88b26121c2e956eca572e1d8b807a3e2338fdd0dc/stage";
@@ -14,11 +15,16 @@ const App = () => {
   useEffect(() => {
     alanBtn({
       key: alankey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles,number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
         }else if(command==='highlight'){
 setActiveArticle((prevActiveArticle)=>prevActiveArticle+1)
+        }else if(command==='open'){
+          const parsedNumber=number.length>2? wordsToNumbers(number,{fuzzy:true}):number;
+          const article=articles[parsedNumber-1];
+          window.open(article.url, '_blank');
+          alanBtn().playText('Opening...')
         }
       },
     });
